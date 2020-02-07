9940c4f3a143621adfb5a314b7b5fde180dd9046
Vinicius Juan <48996337+vinijuan007@users.noreply.github.com>
2020-02-07 14:41:50 +0000

    created two classes: Motor and Movement
    Motor class, is a class where you can define the pins of the motor with additional parameter fuctionality
    Movemnet class will make the movent occur since t will combine the movemnt of the two motors

--- motors.h
+++ motors.h
@@ -10,14 +10,41 @@ protected:
    
 public:
    Motor(PinName d,PinName p) : direction(d), pwm(p){
-       direction = 0 ;// depending on which wheel it will be 1 or 0 
        period = 100;
        pwm.period_ms(period);//setting the period 
-
    }
-   void set_dt(float dc) {
+
+   void set_duty_cycle(float dc) {
    pwm.write(dc); // setting the duty cycle 
+   }
+
+   void set_direction(bool a) {
+   direction = a; // directio the motor will turn 
    }
    
 };
+
+class Movement{
+protected:
+    Motor* RM;
+    Motor* LM;
+
+public:
+    Movement(Motor* r,Motor* l) : RM(r), LM(l){}
+    void Move_foward(float speed){
+        RM->set_duty_cycle(speed);
+        RM->set_direction(1);
+        LM->set_duty_cycle(speed);
+        LM->set_direction(0);
+    }
+    
+};
+
+int main(){
+    Motor* Right_Motor = new Motor(D1,D2);
+    Motor* Left_Motor = new Motor(D3,D4);
+    Movement Move_Buggy(PL , CC, 0.0, 3.3, 1, 10);
+     while (1) {};
+      
+} 
 
