0902dd77928fafd00a1cafc6b9e8b3254ab6eb1e
Vinicius Juan <48996337+vinijuan007@users.noreply.github.com>
2020-02-09 10:26:53 +0000

    added the encoder library

--- main.cpp
+++ main.cpp
@@ -1,52 +1,7 @@
 #include "mbed.h"
 
-class Motor{ 
-private:
-   DigitalOut direction;// direction of rotation
-   DigitalOut polar_mode;//Bipolar or Unipolar mode
-   PwmOut pwm;
-   int period; // period in milli seconds of the pwm signal 
-   float duty_cycle; // number between 0.0f - 1.0f
-   
-public:
-   Motor (PinName d,PinName pm, PinName p)
-   : direction(d), polar_mode(pm), pwm(p){
-       polar_mode = 1;// 1 for bipolar 0 for unipolar (check info)
-       period = 100;
-       pwm.period_ms(period);//setting the period 
-   }
+int main(void){
 
-   void set_duty_cycle(float dc) {
-   pwm.write(dc); // setting the duty cycle 
-   }
-
-   void set_direction(bool a) {
-   direction = a; // directio the motor will turn 
-   }
-   
-};
-
-class Movement{
-protected:
-    Motor* RM;
-    Motor* LM;
-
-public:
-    Movement(Motor* r,Motor* l) : RM(r), LM(l){}
-    void Move_foward(float speed){
-        RM->set_duty_cycle(speed);
-        RM->set_direction(1);
-        LM->set_duty_cycle(speed);
-        LM->set_direction(0);
-    }
-    
-};
-
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
-
-    }
-}
-
+     while (1) {};
+      
+} 
