334e41f82f5da596fb1ac9b9e83b0c44a8dc8458
Vinicius Juan <48996337+vinijuan007@users.noreply.github.com>
2020-02-17 11:26:36 +0000

    calculates distance

--- MoveWithControl.cpp
+++ MoveWithControl.cpp
@@ -1,3 +1,4 @@
+
 #include "mbed.h"
 #include "QEI.h"
 #include "C12832.h"
@@ -18,17 +19,17 @@ float distanceB;
 float time; // amount of time between ticker update of speed 
 float duty_cycleA; 
 float duty_cycleB;
-int a;
+float sum_of_pulsesA;
 
 Ticker tkr;
 
 public:
 Speed_Control(QEI* r, QEI* l): REncoder(r), LEncoder(l){
-    a = 0; //initializing a ;
+    sum_of_pulsesA = 0;
     REncoder->reset();
     LEncoder->reset();
-    time = 0.4;
-    duty_cycleA = 0.6;
+    time = 0.5;
+    duty_cycleA = 0.586;
     duty_cycleB = 0.6;
     Buggy_Motors.set_duty_cycleA(duty_cycleA);
     Buggy_Motors.set_duty_cycleB(duty_cycleB);
@@ -40,61 +41,71 @@ void calculate_speed(void){
     float diameter = 0.08; // diameter of the wheels 
     pulse_a = (float (REncoder->getPulses()))/time;// pulse rate of A
     pulse_b = (float (LEncoder->getPulses()))/time;// pulse rate of B
-    speed_REncoder = pulse_a * 3.141 * diameter / 256;
-    speed_LEncoder = pulse_b * 3.141 * diameter / 256;
+    speed_REncoder = abs(pulse_a) * 3.141 * diameter / 1024;
+    speed_LEncoder = abs(pulse_b) * 3.141 * diameter / 1024;
     REncoder->reset();
     LEncoder->reset();
 }
 
-/*void calculate_distance(void){
-    distanceA = speed_REncoder*time;
-    distanceB = speed_LEncoder*time;
-}*/
+void calculate_distance(void){
+
+    sum_of_pulsesA = sum_of_pulsesA + (float (REncoder->getPulses()));
+    distanceA = sum_of_pulsesA * 3.141 * 0.08 / 1024;
+    lcd.locate(0,20);
+    lcd.printf("%.4f ",distanceA );
+}
 
 void printRM(void){
-    lcd.locate(0,3);
+    lcd.locate(0,2);
     lcd.printf("A");
-    lcd.locate(15,3);
-    lcd.printf("%float ", speed_LEncoder);
+    lcd.locate(0,12);
+    lcd.printf("%.4f ", speed_REncoder);
 }
 
 void printLM(){
-    lcd.locate(0,14);
+    lcd.locate(60,2);
     lcd.printf("B");
-    lcd.locate(15,14);
-    lcd.printf("%float ", speed_REncoder);
+    lcd.locate(60,12);
+    lcd.printf("%.4f ", speed_LEncoder);
 }
 
-void control_speed(void){
-    if(speed_REncoder > speed_LEncoder){
-        duty_cycleA = duty_cycleA - 0.01;
-        duty_cycleB = duty_cycleB + 0.01;
+void control_speedR(void){
+    float error;
+    if(1.3 > speed_REncoder){
+        error = (1.3 - speed_REncoder)*0.5/1.558;
+        duty_cycleA = duty_cycleA + error;
         Buggy_Motors.set_duty_cycleA(duty_cycleA);
-        Buggy_Motors.set_duty_cycleB(duty_cycleB);
+        lcd.locate(0,20);
+        lcd.printf(">>%.4f ", duty_cycleA);
+        if(duty_cycleA > 1){
+            duty_cycleA = 1.0; 
+            }
     }
-    else if(speed_REncoder < speed_LEncoder){
-        duty_cycleA = duty_cycleA + 0.01;
-        duty_cycleB = duty_cycleB - 0.01;
+    else if(1.3 < speed_REncoder){
+        error = (1.3 - speed_REncoder)*0.5/1.558;
+        duty_cycleA = duty_cycleA + error;
         Buggy_Motors.set_duty_cycleA(duty_cycleA);
-        Buggy_Motors.set_duty_cycleB(duty_cycleB);
+        lcd.locate(0,20);
+        lcd.printf("<<%.4f ", duty_cycleA);
+        if(duty_cycleA > 1){
+            duty_cycleA = 1.0; 
+            }
+    
     }
     else {
         Buggy_Motors.set_duty_cycleA(duty_cycleA);
-        Buggy_Motors.set_duty_cycleB(duty_cycleB);
     }
 }
 
 void Move_foward(void){
+    calculate_distance();
     calculate_speed();
-    control_speed();
-    a++;
-    if(a == 3){
-        a = 0;
-        printLM();
-        printRM();
+   // control_speedR();
+    printLM();
+    printRM();
     }
 
-}
+
 
 
 };
@@ -102,10 +113,9 @@ void Move_foward(void){
 
 int main(){
 
-    QEI* Right_Encoder = new QEI(PC_8,PC_6, NC, 1024, QEI::X4_ENCODING);
-    QEI* Left_Encoder = new QEI(PB_15,PB_14, NC, 1024 , QEI::X4_ENCODING);
+    QEI* Right_Encoder = new QEI(PB_15,PB_14, NC, 1024, QEI::X4_ENCODING);
+    QEI* Left_Encoder = new QEI(PC_8,PC_6, NC, 1024 , QEI::X4_ENCODING);
     Speed_Control Speed_of_buggy(Right_Encoder, Left_Encoder);
      while (1) {};
       
 } 
-
